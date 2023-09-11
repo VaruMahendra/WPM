3aa96fadeb572efcc6f4e9cbb506f34e7112f057
VaruMahendra <21eg106b14@anurag.edu.in>
2023-09-11 16:32:24 +0530

    first commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'WELCOME TO AI_B' });
+});
+
+module.exports = router;
